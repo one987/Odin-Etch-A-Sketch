af3f2bb5f12e66d47a3196f0530c7a71d6dba333
one987 <joelbed@gmail.com>
2022-10-17 13:38:49 -0400

    function that generates the grid based on row and column parameters

--- javascript.js
+++ javascript.js
@@ -1,28 +1,32 @@
 
 
-const container = document.querySelector('#container');
+const container = document.querySelector('#main');
 const grid = document.createElement('div');
 grid.classList.add('grid');
-let columns = 16;
-let rows = 16;
 
 
 
-
-//create function that loads a grid of divs
-function createGrid() {
-}
+//function that generates a grid of divs
+function createGrid(c, r) {
+    let columns = c;
+    let rows = r;
 
 for (let i = 0; i < columns; ++i) {
     let column = document.createElement('div');
     column.className = 'column';
+    grid.style.gridTemplateColumns = `repeat(${c}, 1fr)`;
+    
     for (let j = 0; j < rows; ++j) {
         let row = document.createElement('div');
         row.className = 'row';
-        
+        grid.style.gridTemplateRows = `repeat(${r}, 1fr)`;
         column.appendChild(row);
     }
     grid.appendChild(column);
+    
 }
-container.appendChild(grid);
+main.appendChild(grid);
 
+}
+
+createGrid(16,16);
