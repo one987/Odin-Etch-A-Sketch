7fc43d794c097a5ba5f98954ea58e65731eaba2e
one987 <joelbed@gmail.com>
2022-10-20 00:34:21 -0400

    cleaned up createGrid function

--- javascript.js
+++ javascript.js
@@ -18,43 +18,40 @@ slider.oninput = function () {
 //function that generates a grid of divs. Need to refactor to fix scope issues
 function createGrid(cr) {
 
-    for (let i = 0; i < cr; ++i) {
-        let column = document.createElement('div');
-        column.className = 'column';
-        grid.style.gridTemplateColumns = `repeat(${cr}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${cr}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${cr}, 1fr)`;
 
-        for (let j = 0; j < cr; ++j) {
-            let row = document.createElement('div');
-            row.className = 'row';
-            grid.style.gridTemplateRows = `repeat(${cr}, 1fr)`;
-            row.addEventListener("mouseover", () => {
-                row.style.backgroundColor = 'blue';
-            });
-            const clear = document.querySelector('#clear'); //reset button
-            clear.addEventListener('click', () => {
-                row.style.backgroundColor = '#F8F8FF';
-            });
-            const black = document.querySelector('#black'); //black button
-            black.addEventListener('click', () => {
-                row.addEventListener("mouseover", () => {
-                    row.style.backgroundColor = 'black';
-                });
-            });
-            const erase = document.querySelector('#erase'); //erase button
-            erase.addEventListener('click', () => {
-                row.addEventListener("mouseover", () => {
-                    row.style.backgroundColor = '#F8F8FF';
-                });
-            });
+    for (let i = 0; i < cr * cr; ++i) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
 
-            column.appendChild(row);
-        }
-        grid.appendChild(column);
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = 'blue';
+        });
+        const clear = document.querySelector('#clear'); //reset button
+        clear.addEventListener('click', () => {
+            cell.style.backgroundColor = '#F8F8FF';
+        });
+        const black = document.querySelector('#black'); //black button
+        black.addEventListener('click', () => {
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = 'black';
+            });
+        });
+        const erase = document.querySelector('#erase'); //erase button
+        erase.addEventListener('click', () => {
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = '#F8F8FF';
+            });
+        });
+
+        grid.appendChild(cell);
     }
     main.appendChild(grid);
 }
 
-window.onload = function () {
-    createGrid(64);
-};
+
+
+createGrid(64);
+
 
